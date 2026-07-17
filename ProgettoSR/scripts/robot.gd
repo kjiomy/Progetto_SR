e7e233d0a8331d0e5d3569f7dea3c1eb55c7d192
kjiomy <kimjoy.randazzo12@gmail.com>
2026-07-17 16:16:08 +0200

    fix: problemi generali nella traiettoria

--- ProgettoSR/scripts/robot.gd
+++ ProgettoSR/scripts/robot.gd
@@ -8,42 +8,29 @@ func _ready() -> void:
 	DDS.subscribe("Torque")
 	DDS.subscribe("Theta")
 
-func _process(delta: float) -> void:
-	DDS.publish("tick", DDS.DDS_TYPE_FLOAT, delta)
-	DDS.publish("X", DDS.DDS_TYPE_FLOAT, global_position.x)
-	DDS.publish("Z", DDS.DDS_TYPE_FLOAT, global_position.z)
-	
-	# FIX: Otteniamo il vettore frontale 3D del veicolo (-Z in Godot)
-	var forward_dir = global_transform.basis.z
-	# Calcoliamo lo yaw 2D usando atan2, che combacerà esattamente con il math.atan2 di Python
-	var yaw_2d = atan2(forward_dir.z, forward_dir.x)
-	DDS.publish("Yaw", DDS.DDS_TYPE_FLOAT, yaw_2d)
-	
-	var current_speed = linear_velocity.length()
-	DDS.publish("Speed", DDS.DDS_TYPE_FLOAT, current_speed)
+var time_passed: float = 0.0
+var publish_rate: float = 0.05
 
+func _physics_process(delta: float) -> void:
 	var torque = DDS.read("Torque")
 	var theta = DDS.read("Theta")
-	var brake_max = 100
+	var brake_max = 200
 	
-	if theta != 0 and theta != null:
-			var theta_abs_rad = deg_to_rad(abs(theta))
-			var theta_tan = tan(theta_abs_rad)
-			var modifier = (W / (2 * L)) * theta_tan
+	if theta != null and theta != 0:
+		var theta_abs_rad = deg_to_rad(abs(theta))
+		var theta_tan = tan(theta_abs_rad)
+		var modifier = (W / (2 * L)) * theta_tan
 
-			var inner_theta = atan(theta_tan / (1 - modifier))
-			var outer_theta = atan(theta_tan / (1 + modifier))
-			
-			if theta > 0:
-				# Giriamo a sinistra, la ruota sinistra e' interna.
-				$Wheel_FL.steering = inner_theta
-				$Wheel_FR.steering = outer_theta
-			else:
-				# Giriamo a destra , la ruota destra e' interna
-				$Wheel_FR.steering = -inner_theta
-				$Wheel_FL.steering = -outer_theta
+		var inner_theta = atan(theta_tan / (1 - modifier))
+		var outer_theta = atan(theta_tan / (1 + modifier))
+		
+		if theta > 0:
+			$Wheel_FL.steering = inner_theta
+			$Wheel_FR.steering = outer_theta
+		else:
+			$Wheel_FR.steering = -inner_theta
+			$Wheel_FL.steering = -outer_theta
 	else:
-		# Se andiamo dritti azzeriamo lo sterzo di entrambe le ruote
 		$Wheel_FL.steering = 0.0
 		$Wheel_FR.steering = 0.0
 	
@@ -54,3 +41,19 @@ func _process(delta: float) -> void:
 		else:
 			self.set_brake(0.0)
 			self.set_engine_force(torque)
+
+
+	time_passed += delta
+	if time_passed >= publish_rate:
+		time_passed = 0.0 
+		
+		DDS.publish("tick", DDS.DDS_TYPE_FLOAT, delta)
+		DDS.publish("X", DDS.DDS_TYPE_FLOAT, global_position.x)
+		DDS.publish("Z", DDS.DDS_TYPE_FLOAT, global_position.z)
+		
+		var forward_dir = global_transform.basis.z
+		var yaw_2d = atan2(forward_dir.x, forward_dir.z)
+		DDS.publish("Yaw", DDS.DDS_TYPE_FLOAT, yaw_2d)
+		
+		var current_speed = linear_velocity.length()
+		DDS.publish("Speed", DDS.DDS_TYPE_FLOAT, current_speed)
