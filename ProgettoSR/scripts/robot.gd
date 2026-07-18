8335d09f21c6e1e16cc89b5295f7cf3f50a4642f
kjiomy <kimjoy.randazzo12@gmail.com>
2026-07-18 11:00:38 +0200

    feat: aggiunto un array di punti su godot per la gestione della navigazione del labirinto

--- ProgettoSR/scripts/robot.gd
+++ ProgettoSR/scripts/robot.gd
@@ -2,11 +2,22 @@ extends VehicleBody3D
 @onready var L : float = abs($Wheel_FL.position.z - $Wheel_BL.position.z) # Distanza tra ruote anteriori e posteriori
 @onready var W : float = abs($Wheel_FL.position.x - $Wheel_FR.position.x) # Distanza tra le ruote sterzanti
 
+var punti = [
+	[0.0, 10.0],
+	[10.0, 10.0],
+	[10.0, 0.0],
+	[0.0, 0.0],
+	[-15, -15]
+]
+
+var indice = 0;
+
 
 func _ready() -> void:
 	self.can_sleep = false
 	DDS.subscribe("Torque")
 	DDS.subscribe("Theta")
+	DDS.subscribe("Index")
 
 var time_passed: float = 0.0
 var publish_rate: float = 0.05
@@ -14,6 +25,14 @@ var publish_rate: float = 0.05
 func _physics_process(delta: float) -> void:
 	var torque = DDS.read("Torque")
 	var theta = DDS.read("Theta")
+	var index = DDS.read("Index")
+	
+	if index != null:
+		indice = index
+	else:
+		pass
+	
+	
 	var brake_max = 200
 	
 	if theta != null and theta != 0:
@@ -57,3 +76,6 @@ func _physics_process(delta: float) -> void:
 		
 		var current_speed = linear_velocity.length()
 		DDS.publish("Speed", DDS.DDS_TYPE_FLOAT, current_speed)
+		DDS.publish("X_dest", DDS.DDS_TYPE_FLOAT, punti[indice][0])
+		DDS.publish("Z_dest", DDS.DDS_TYPE_FLOAT, punti[indice][1])
+		DDS.publish("Indice", DDS.DDS_TYPE_INT, indice)
